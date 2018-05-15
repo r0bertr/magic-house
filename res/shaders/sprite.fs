2cc429fe7924de0167aa585eaf229ce8dcbc5f6a
HinanawiTenshi <dr.paper@live.com>
2018-05-15 21:57:05 +0800

    Update shader sprite

--- res/shaders/sprite.fs
+++ res/shaders/sprite.fs
@@ -1,10 +1,27 @@
 #version 330 core
 in vec2 TexCoords;
-out vec4 color;
+in vec3 Normal;
+out vec4 fragColor;
 
-uniform sampler2D image;
-uniform vec3 spriteColor;
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform sampler2D texture3;
+uniform sampler2D texture4;
+uniform sampler2D texture5;
+uniform sampler2D texture6;
+uniform sampler2D texture7;
+uniform sampler2D texture8;
+uniform sampler2D texture9;
+uniform sampler2D texture10;
+uniform sampler2D texture11;
+uniform sampler2D texture12;
+uniform sampler2D texture13;
+uniform sampler2D texture14;
+uniform sampler2D texture15;
+
+uniform vec3 color;
 
 void main() {
-    color = vec4(spriteColor, 1.f);
+    fragColor = texture(texture0, TexCoords);
 }
--- res/shaders/sprite.vs
+++ res/shaders/sprite.vs
@@ -1,8 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec2 tex;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 tex;
 
 out vec2 TexCoords;
+out vec3 Normal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -10,5 +12,6 @@ uniform mat4 projection;
 
 void main() {
     TexCoords = tex;
+    Normal = normal;
     gl_Position = projection * view * model * vec4(pos, 1.f);
 }
