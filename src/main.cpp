186cd9e1899bf76419a917dfeb1a927c3adcdf45
HinanawiTenshi <dr.paper@live.com>
2018-05-22 13:33:43 +0800

    Update main.cpp Add mouse free looking

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,43 @@
 const GLuint WIDTH = 800;
 const GLuint HEIGHT = 600;
 
+void mouseCallback(GLFWwindow *window, double xpos, double ypos) {
+    static bool firstMouse = true;
+    static float lastX = 0.f, lastY = 0.f;
+    static float yaw = -90.f, pitch = 0.f;
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = .01f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    ResourceManager::GetInstance()->getCamera("main")->rotate(pitch, yaw);
+}
+
 int main() {
     Game game(WIDTH, HEIGHT);
     game.init();
+
+    glfwSetInputMode(game.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(game.getWindow(), mouseCallback);
 
     // Main Loop
     while (!glfwWindowShouldClose(game.getWindow())) {
