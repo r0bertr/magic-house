550b0748fcaef8ec71b5c1c398de1d90e3a4b41f
Richard4Yjl <yuanjliang@mail2.sysu.edu.cn>
2018-06-12 15:21:39 +0800

    使用GLFW自带的函数做多采样缓冲达到抗锯齿

--- src/Game.cpp
+++ src/Game.cpp
@@ -11,6 +11,7 @@ Game::Game(GLuint width, GLuint height) {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
